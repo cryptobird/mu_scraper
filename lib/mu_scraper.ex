ff1ef7185e1f4e98a14cd311a95d94d9c2526ad7
cryptobird <parrott@Parrottbook.PK5001Z>
2016-08-29 09:22:48 -0600

    pretty much done tho

--- lib/mu_scraper.ex
+++ lib/mu_scraper.ex
@@ -15,6 +15,19 @@ defmodule MuScraper do
       |> List.last
       |> Floki.find("a")
       |> Enum.map(&Floki.text/1)
-      |> Enum.each(&IO.puts/1)
+      |> Enum.map(&song_parse/1)
+   end
+
+   def song_parse(song) do
+      [_ | tail] = List.flatten(Regex.scan(~r/(.+)–(.+)via/, song))
+      [artist | title] = tail
+      song = %{artist: String.trim(artist), title: String.trim(List.first(title))}
+      Map.put(song, :gp_link, google_play_link(song))
+   end
+
+   def google_play_link(song) do
+      title = Regex.replace(~r/\s/, song.title, "+")
+      artist = Regex.replace(~r/\s/, song.artist, "+")
+      "https://play.google.com/music/listen?u=0#/sr/#{title}+##{artist}" 
    end
 end
